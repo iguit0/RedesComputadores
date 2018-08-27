39b406bba173a587353126310b0997bbe463e419
Matheus Rocha Vieira <matheusrv@email.com>
2018-08-26 22:07:26 -0300

    Server Implementation Improve

--- serverclient-socket/jsonsocket.py
+++ serverclient-socket/jsonsocket.py
@@ -17,7 +17,7 @@ class Server(object):
   client = None
 
   def __init__(self, host, port):
-    self.socket = socket.socket()
+    self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.socket.bind((host, port))
     self.socket.listen(self.backlog)
 
--- serverclient-socket/server.py
+++ serverclient-socket/server.py
@@ -6,20 +6,24 @@ import json
 host = 'localhost'
 port = 8080
 srv = 1
+CONNECTION_LIST = [] # Mantém uma lista dos sockets ativos
 
 server = Server(host, port)
 
 while True:
-	#server.accept()
-	#data = server.recv()
-	data = server.accept().recv()
+
+	server.accept()
+	CONNECTION_LIST.append(server.accept())
+	data = server.recv()
+	#data = server.accept().recv()
 	print(data)
 	server.send({'status': 'ok'})
-	a = any(d['dist'] == 2 for d in data)
-	if a:
-		client = Client()
-		print(data)
-		response = client.connect(host, 8081).send(data).recv()
-		print(response)
-		client.close()
-		time.sleep(10)
+	#a = filter(lambda data: data['dist'] == srv, data)
+	#print(a)
+	#if a:
+	#	client = Client()
+	#	print(data)
+	#	response = client.connect(host, 8081).send(data).recv()
+	#	print(response)
+	#	client.close()
+	#	time.sleep(10)
