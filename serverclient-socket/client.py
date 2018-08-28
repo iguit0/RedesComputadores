553693483fd6f241bc9525ae4855c9dfc0d612c7
Matheus Rocha Vieira <matheusrv@email.com>
2018-08-28 15:29:41 -0300

    [0.1.0] Working Version: Send & Receive With Username@Domain

--- serverclient-socket/client.py
+++ serverclient-socket/client.py
@@ -17,25 +17,25 @@ srvlist = {
 	}
 }
 
-srv = input('University:')
-host = srvlist[srv]['ip']
-port = srvlist[srv]['port']
+emitterDomain = input('University:')
+emitterUser = input('Sur Username:')
+host = srvlist[emitterDomain]['ip']
+port = srvlist[emitterDomain]['port']
 
 while True:
+	client = Client()
 	print('Program Name\nType 1 for Send\nType 2 to Receive')
 	menu = input('Option: ')
 	if menu == '1':
-		client = Client()
-		user = input('User:')
-		domain = input('University: ')
+		recipientUser = input('To:')
+		recipientDomain = input('University: ')
 		msg = input('Message:')
 		data = {
 			'act': 'send',
-			'domain': domain,
-			'user': user,
+			'recipientDomain': recipientDomain,
+			'recipientUser': recipientUser,
 			'msg': msg
 		}
-		print(data)
 		response = client.connect(host, port).send(data).recv()
 		client.close()
 		if response['status'] == '200':
@@ -44,8 +44,10 @@ while True:
 			print('Erro ao Transmitir Mensagem')
 
 	if menu == '2':
-		server = Server(host, port)
-		data = server.accept().recv()
-		print(data)
-		server.send({'status': 'ok'})
-		server.close()
+		data = {
+			'act': 'receive',
+			'requesterUser': emitterUser
+		}
+		response = client.connect(host, port).send(data).recv()
+		print(response)
+		client.close()
--- serverclient-socket/server.py
+++ serverclient-socket/server.py
@@ -16,37 +16,45 @@ srvlist = {
 	}
 }
 
-srv = input('University:')
-host = srvlist[srv]['ip']
-port = srvlist[srv]['port']
+serverDomain = input('University:')
+host = srvlist[serverDomain]['ip']
+port = srvlist[serverDomain]['port']
 
 server = Server(host, port)
 
-storemail = []
+msgList = []
 
 while True:
 	#print('\tNAME: ' + str(server.socket..gethostname()))
 	#print('\tIP: ' + str(server.socket.gethostbyname(server.socket.gethostname())))
 	#print('\tPORT:' + str(port))
-	#print('\tSERVER: ' + str(srv))
+	#print('\tSERVER: ' + str(emitterDomain))
 	#server.accept()
 	data = server.accept().recv()
 	#data = server.recv()
 	print(data)
-	server.send({'status': '200'})
-	if data['domain'] == srv:
-		data['status'] = '202'
-		storemail.append(data)
-	print(storemail)
-	if data['domain'] != srv:
-		print('Enviando mensagem para server: ' + str(data['domain']))
-		client = Client()
-		data['status'] = '201'
-		hostDomain = data['domain']
-		response = client.connect(srvlist[hostDomain]['ip'], srvlist[hostDomain]['port']).send(data).recv()
-		client.close()
-		if response['status'] == '200':
-			print('Mensagem Transmitida com Sucesso')
-		else:
-			print('Erro ao Transmitir Mensagem')
+	if data['act'] == 'send':
+		server.send({'status': '200'})
+		if data['recipientDomain'] == serverDomain:
+			data['status'] = '202'
+			msgList.append(data)
+		print(msgList)
+		if data['recipientDomain'] != serverDomain:
+			print('Enviando mensagem para server: ' + str(data['recipientDomain']))
+			client = Client()
+			data['status'] = '201'
+			response = client.connect(srvlist[data['recipientDomain']]['ip'], srvlist[data['recipientDomain']]['port']).send(data).recv()
+			client.close()
+			if response['status'] == '200':
+				print('Mensagem Transmitida com Sucesso')
+			else:
+				print('Erro ao Transmitir Mensagem')
+	if data['act'] == 'receive':
+		requesterMsg = []
+		for i in msgList:
+			if i['recipientUser'] == data['requesterUser']:
+				requesterMsg.append(i)
+				msgList.remove(i)
+		server.send(requesterMsg)
+
 
