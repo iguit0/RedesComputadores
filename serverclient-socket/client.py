cf0645fdc4eebb3c88f16bfeb7e9ace127bf069a
Matheus Rocha Vieira <matheusrv@email.com>
2018-08-28 17:01:14 -0300

    Some Output Prints

--- serverclient-socket/client.py
+++ serverclient-socket/client.py
@@ -1,20 +1,19 @@
-#file:client.py
+import os
 from jsonsocket import Client, Server
-import time
 
 srvlist = {
-	"ufv":
-	{
-		"id": 1,
-		"ip": 'localhost',
-		"port": 8888
-	},
-	"ufla":
-	{
-		"id": 2,
-		"ip": 'localhost',
-		"port": 8889
-	}
+    "ufv":
+    {
+        "id": 1,
+        "ip": 'localhost',
+        "port": 8888
+    },
+    "ufla":
+    {
+        "id": 2,
+        "ip": 'localhost',
+        "port": 8889
+    }
 }
 
 emitterDomain = input('University:')
@@ -23,31 +22,41 @@ host = srvlist[emitterDomain]['ip']
 port = srvlist[emitterDomain]['port']
 
 while True:
-	client = Client()
-	print('Program Name\nType 1 for Send\nType 2 to Receive')
-	menu = input('Option: ')
-	if menu == '1':
-		recipientUser = input('To:')
-		recipientDomain = input('University: ')
-		msg = input('Message:')
-		data = {
-			'act': 'send',
-			'recipientDomain': recipientDomain,
-			'recipientUser': recipientUser,
-			'msg': msg
-		}
-		response = client.connect(host, port).send(data).recv()
-		client.close()
-		if response['status'] == '200':
-			print('Mensagem Transmitida com Sucesso')
-		else:
-			print('Erro ao Transmitir Mensagem')
+    os.system("clear")
+    client = Client()
+    print('Program Name\nType 1 for Send\nType 2 to Receive')
+    menu = input('Option: ')
+    if menu == '1':
+        recipientUser = input('Recipient User:')
+        recipientDomain = input('Recipient University: ')
+        msg = input('Message:')
+        data = {
+            'act': 'send',
+            'recipientDomain': recipientDomain,
+            'recipientUser': recipientUser,
+            'emitterDomain': emitterDomain,
+            'emitterUser': emitterUser,
+            'msg': msg
+        }
+        response = client.connect(host, port).send(data).recv()
+        client.close()
+        if response['status'] == '200':
+            print('Success Sending Msg')
+        else:
+            print('Msg Send Error')
 
-	if menu == '2':
-		data = {
-			'act': 'receive',
-			'requesterUser': emitterUser
-		}
-		response = client.connect(host, port).send(data).recv()
-		print(response)
-		client.close()
+    if menu == '2':
+        data = {
+            'act': 'receive',
+            'requesterUser': emitterUser
+        }
+        response = client.connect(host, port).send(data).recv()
+        #if response['status'] == '200':
+        #print('There are:'+response.index()+'new msg')
+        for i in response:
+            #print('EMAIL #'+i.index())
+            print('Emitter: ' + i['emitterUser'] + '@' + i['emitterDomain'])
+            print('Msg: ' + i['msg'])
+        else:
+            print('No New Msg')
+        client.close()
--- serverclient-socket/server.py
+++ serverclient-socket/server.py
@@ -1,19 +1,19 @@
+import os
 from jsonsocket import Server, Client
 
-
 srvlist = {
-	"ufv":
-	{
-		"id": 1,
-		"ip": 'localhost',
-		"port": 8888
-	},
-	"ufla":
-	{
-		"id": 2,
-		"ip": 'localhost',
-		"port": 8889
-	}
+    "ufv":
+    {
+        "id": 1,
+        "ip": 'localhost',
+        "port": 8888
+    },
+    "ufla":
+    {
+        "id": 2,
+        "ip": 'localhost',
+        "port": 8889
+    }
 }
 
 serverDomain = input('University:')
@@ -25,36 +25,40 @@ server = Server(host, port)
 msgList = []
 
 while True:
-	#print('\tNAME: ' + str(server.socket..gethostname()))
-	#print('\tIP: ' + str(server.socket.gethostbyname(server.socket.gethostname())))
-	#print('\tPORT:' + str(port))
-	#print('\tSERVER: ' + str(emitterDomain))
-	#server.accept()
-	data = server.accept().recv()
-	#data = server.recv()
-	print(data)
-	if data['act'] == 'send':
-		server.send({'status': '200'})
-		if data['recipientDomain'] == serverDomain:
-			data['status'] = '202'
-			msgList.append(data)
-		print(msgList)
-		if data['recipientDomain'] != serverDomain:
-			print('Enviando mensagem para server: ' + str(data['recipientDomain']))
-			client = Client()
-			data['status'] = '201'
-			response = client.connect(srvlist[data['recipientDomain']]['ip'], srvlist[data['recipientDomain']]['port']).send(data).recv()
-			client.close()
-			if response['status'] == '200':
-				print('Mensagem Transmitida com Sucesso')
-			else:
-				print('Erro ao Transmitir Mensagem')
-	if data['act'] == 'receive':
-		requesterMsg = []
-		for i in msgList:
-			if i['recipientUser'] == data['requesterUser']:
-				requesterMsg.append(i)
-				msgList.remove(i)
-		server.send(requesterMsg)
+    os.system("clear")
+    #print('\tNAME: ' + str(server.socket..gethostname()))
+    #print('\tIP: ' + str(server.socket.gethostbyname(server.socket.gethostname())))
+    #print('\tPORT:' + str(port))
+    #print('\tSERVER: ' + str(emitterDomain))
+    #server.accept()
+    #data = server.recv()
+    data = server.accept().recv()
+    print('Msg Memory: ' + str(msgList))
+    print('Socket Receive Data: '+str(data))
+    if data['act'] == 'send':
+        server.send({'status': '200'})
+        if data['recipientDomain'] == serverDomain:
+            data['status'] = '202'
+            msgList.append(data)
+        if data['recipientDomain'] != serverDomain:
+            print('Transferring Msg: ' + str(data['recipientDomain']))
+            client = Client()
+            data['status'] = '201'
+            response = client.connect(srvlist[data['recipientDomain']]['ip'], srvlist[data['recipientDomain']]['port']).send(data).recv()
+            client.close()
+            if response['status'] == '200':
+                print('Transfered Msg Success')
+            else:
+                print('Transfered Msg Error')
+    if data['act'] == 'receive':
+        requesterMsg = []
+        for i in msgList:
+            if i['recipientUser'] == data['requesterUser']:
+                requesterMsg.append(i)
+                msgList.remove(i)
+        server.send(requesterMsg)
+    if (data['act'] != 'receive') & (data['act'] != 'send'):
+        server.send('ação desconhecida')
+        print('ação desconhecida')
 
 
