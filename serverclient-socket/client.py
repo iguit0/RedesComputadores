edd4093c52cf24a13c908ddf84962657f540db2b
Matheus Rocha Vieira <matheusrv@email.com>
2018-08-28 12:46:09 -0300

    Code Features Implementation: New way to discover destination

--- serverclient-socket/client.py
+++ serverclient-socket/client.py
@@ -2,26 +2,43 @@
 from jsonsocket import Client, Server
 import time
 
-host = 'localhost'
-port = 8080
+srvlist = {
+	"ufv":
+	{
+		"id": 1,
+		"ip": 'localhost',
+		"port": 8888
+	},
+	"ufla":
+	{
+		"id": 2,
+		"ip": 'localhost',
+		"port": 8889
+	}
+}
 
+srv = input('University:')
+host = srvlist[srv]['ip']
+port = srvlist[srv]['port']
 
 while True:
 	print('Program Name\nType 1 for Send\nType 2 to Receive')
 	menu = input('Option: ')
 	if menu == '1':
 		client = Client()
-		#dest = input('Destination: ')
-		msg = input('Type your message:')
+		user = input('User:')
+		domain = input('University: ')
+		msg = input('Message:')
 		data = {
 			'act': 'send',
-			'dest': 2,
+			'domain': domain,
+			'user': user,
 			'msg': msg
 		}
 		print(data)
 		response = client.connect(host, port).send(data).recv()
 		client.close()
-		if response['status'] == 'ok':
+		if response['status'] == '200':
 			print('Mensagem Transmitida com Sucesso')
 		else:
 			print('Erro ao Transmitir Mensagem')
--- serverclient-socket/server.py
+++ serverclient-socket/server.py
@@ -1,26 +1,24 @@
-#file:server.py
-import numpy as np
 from jsonsocket import Server, Client
-import time
 
-host = ''
-port = 8081
-srv = 2
 
 srvlist = {
-	"server1":
+	"ufv":
 	{
 		"id": 1,
 		"ip": 'localhost',
-		"port": "8080"
+		"port": 8888
 	},
-	"server2":
+	"ufla":
 	{
 		"id": 2,
 		"ip": 'localhost',
-		"port": "8081"
+		"port": 8889
 	}
 }
+
+srv = input('University:')
+host = srvlist[srv]['ip']
+port = srvlist[srv]['port']
 
 server = Server(host, port)
 
@@ -30,22 +28,24 @@ while True:
 	#print('\tNAME: ' + str(server.socket..gethostname()))
 	#print('\tIP: ' + str(server.socket.gethostbyname(server.socket.gethostname())))
 	#print('\tPORT:' + str(port))
-	print('\tSERVER: ' + str(srv))
+	#print('\tSERVER: ' + str(srv))
 	#server.accept()
 	data = server.accept().recv()
 	#data = server.recv()
 	print(data)
-	server.send({'status': 'ok'})
-	if data['act'] == 'transfer':
+	server.send({'status': '200'})
+	if data['domain'] == srv:
+		data['status'] = '202'
 		storemail.append(data)
 	print(storemail)
-	if data['dest'] != srv:
-		print('Enviando mensagem para server: ' + str(data['dest']))
+	if data['domain'] != srv:
+		print('Enviando mensagem para server: ' + str(data['domain']))
 		client = Client()
-		data['act'] = 'transfer'
-		response = client.connect(host, 8081).send(data).recv()
+		data['status'] = '201'
+		hostDomain = data['domain']
+		response = client.connect(srvlist[hostDomain]['ip'], srvlist[hostDomain]['port']).send(data).recv()
 		client.close()
-		if response['status'] == 'ok':
+		if response['status'] == '200':
 			print('Mensagem Transmitida com Sucesso')
 		else:
 			print('Erro ao Transmitir Mensagem')
