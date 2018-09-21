db38aea8954b1e3f8af11b1b4b3c94e901190101
Igor Alves <igorsk89@gmail.com>
2018-09-21 02:38:11 -0300

    metodo de verificacao checksum

--- udp-rdt/funcoes.py
+++ udp-rdt/funcoes.py
@@ -30,3 +30,16 @@ def menu(vetor):
 def complemento(n, tamanho):
     comp = n ^ ((1 << tamanho) - 1)
     return '0b{0:0{1}b}'.format(comp, tamanho)
+
+#Soma de verificação
+def checksum(portaorigem,portadestino,comprimento):
+	primeirasoma = bin(portaorigem+portadestino)[2:].zfill(16)
+	if (len(primeirasoma)>16):
+		primeirasoma = primeirasoma[1:17]
+		primeirasoma = bin(int(primeirasoma,2) + 1)[2:].zfill(16)
+	segundasoma = bin(int(primeirasoma,2)+comprimento)[2:].zfill(16)
+	if (len(segundasoma)>16):
+		segundasoma = segundasoma[1:17]
+		segundasoma = bin(int(segundasoma,2) + 1)[2:].zfill(16)
+	checksum = complemento(int(segundasoma,2),16)[2:]
+	return int(checksum,2)
